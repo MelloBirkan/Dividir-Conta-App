e45719e8b09e70430a21281d597fb0e51b34c71d
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2024-07-22 22:44:01 -0300

    Initial Commit

---
+++ Dividir Conta/Dividir_ContaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dividir_ContaApp.swift
+//  Dividir Conta
+//
+//  Created by Marcello Gonzatto Birkan on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Dividir_ContaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
